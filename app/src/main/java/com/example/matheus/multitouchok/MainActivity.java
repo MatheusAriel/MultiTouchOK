56f01573df7ef938a79faecec4aad47a6c0325b5
Matheus Ariel <matheusariel@gmail.com>
2017-09-06 19:27:44 -0300

    TEST COMMIT

--- app/src/main/java/com/example/matheus/multitouchok/MainActivity.java
+++ app/src/main/java/com/example/matheus/multitouchok/MainActivity.java
@@ -9,7 +9,7 @@ public class MainActivity extends AppCompatActivity
     @Override
     protected void onCreate (Bundle savedInstanceState)
     {
-        //MAIN VAZIA
+        //MAIN VAZIA COMMIT
         super.onCreate (savedInstanceState);
         setContentView (R.layout.activity_main);
     }
